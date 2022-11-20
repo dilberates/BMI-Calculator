930b25b09a0886a46a8cf5f3e805d946e1cd2a7a
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2022-11-20 19:29:38 +0300

    Add-Master
    Add project

---
+++ BMI Calculator/Controllers/ViewController.swift
@@ -0,0 +1,34 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var kiloSlider: UISlider!
+    @IBOutlet weak var boySlider: UISlider!
+    @IBOutlet weak var kiloTxt: UILabel!
+    @IBOutlet weak var boyTxt: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func boyChangeSlider(_ sender: UISlider) {
+        boyTxt.text=(String(format: "%.2f", sender.value)+"m")
+    }
+    
+    @IBAction func kiloChangeSlider(_ sender: UISlider) {
+        
+        kiloTxt.text=(String(format:"%.0f", sender.value)+"kg")
+    }
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let boy=boySlider.value;
+        let kilo=kiloSlider.value;
+        print(boy)
+        print(kilo)
+        let bmi = kilo/(boy*boy)
+        print(bmi)
+    }
+    
+}
+

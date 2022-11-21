4efe3fc75a2aceef502180a85919dc59ed847886
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2022-11-21 22:57:01 +0300

    Update
    Create SecondViewController.swift file.

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Dilber KILIÇ on 21.11.2022.
+//
+
+import UIKit
+
+class SecondViewContoller:UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
